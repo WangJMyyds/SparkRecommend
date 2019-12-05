0a6238d321dcba83c01a7dd0bcd0e44106398f1e
wangjinming <wangjinming123>
2019-12-05 10:59:16 +0800

    提交

---
+++ src/main/scala/com/itstart/dataLoad/DataLoader.scala
@@ -0,0 +1,5 @@
+package com.itstart.dataLoad
+
+object DataLoader {
+
+}
---
+++ src/test/scala/com/itstart/AppTest.scala
@@ -0,0 +1,17 @@
+package com.itstart
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
